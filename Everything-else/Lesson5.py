22df9f8206b271d43152034ca1a89a0c3637c4d4
deddamir77 <deddamir77@gmail.com>
2023-06-12 15:42:41 -0500

    6/12

--- Everything-else/Lesson5.py
+++ Everything-else/Lesson5.py
@@ -6,3 +6,87 @@
     #else:
         #print(car.title())
 
+#requested_topping = 'salami'
+
+#if requested_topping != 'tomatoes': #  The symbol "!=" in Python is used to check for inequality. It is a comparison operator that evaluates to True if the two operands being compared are not equal, and False if they are equal.
+    #print("Hold the tomatoes!")
+#else:
+    #print("Heres your salami.")
+
+#do in jupiter notebook
+#age = '18' # age = '18': This line assigns the string value '18' to the variable age. The variable age is now a string type variable containing the value '18'.
+#age == '18'# age == '18': This line is a comparison expression that checks if the value stored in the variable age is equal to the string '18'. The == operator is the equality operator in Python and returns True if the two operands are equal, and False otherwise.
+
+#age = 19
+#age < 21 # true
+#age <= 21 # true
+#age > 21 # false 
+#age >= 21 # false
+
+#age_1 = 21
+#age_2 = 18
+
+#age_1 >= 20 and age_2 >= 20 # the and function combines both and says that both will be true, but if one  is incorrect the whole statement is false.
+
+#age_1 = 21
+#age_2 = 18
+
+#age_1 >= 20 or age_2 >= 20 # the or function checks for either one of them to be true. 
+
+#requested_toppings = ['pepperoni', 'salami', 'cheese', ' corn', 'pinaple']
+
+#'tomatoes' in requested_toppings # false. function in checks your list if you have that in the list 
+
+#'salami' in requested_toppings #true 
+
+#banned_user = ['PWR', 'TG_Plays', 'Sundee']
+#user = 'the_iron-boy'
+
+#if user not in banned_user:
+    #print(f'{user.title()} you are about to be banned.')
+
+#age = 17.99999999999999
+#if age >= 18:
+    #print("You are old enough to play Call of Duty")
+    #print("Just don't cheat")
+#else:
+    #print("Sorry just have to wait for your birthday in .00000000000000000000000000000000000000000000000000001 seconds")
+    #print("Just turn of this tab an restart")
+
+#age = 12
+
+#if age < 9:
+    #print("You're haircut costs 9 bucks.")
+#elif age < 13:
+    #print("You're haircut cost 13 bucks ")
+#else:
+    #print("You're haircut cost 18 bucks")
+
+#age = 1000000000000
+
+#if age < 4:
+    #price = 0
+#elif age < 12:
+    #price = 13.25
+#elif age < 16:
+    #price = 21.99
+#elif age < 65:
+    #price = 24.45
+#elif age > 65:
+    #price = 20.00
+#else:
+    #price = 29.65
+    
+#print(f"Your haircut is ${price}")
+
+
+#opptional_toppings = ['pepperoni', 'salami', 'chicken', 'parmazon', 'extra_cheese', 'pinapple']
+
+#if 'tomatoes'  in opptional_toppings:
+    #print("Adding tomatoes")
+#if 'corn'  in opptional_toppings:
+    #print("Adding corn")
+#if 'extra_cheese' not in opptional_toppings:
+    #print("Do you want more cheese")
+
+#print("\nYou already have all the toppings, do you wnat more?")
