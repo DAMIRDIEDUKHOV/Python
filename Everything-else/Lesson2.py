2856506a042be780033a7d2b157fbe39938c3961
deddamir77 <deddamir77@gmail.com>
2023-06-12 14:25:00 -0500

    6/12

---
+++ Everything-else/Lesson2.py
@@ -0,0 +1,34 @@
+#first = " Tyler"
+#last = "Broc"
+#fullname = f"{first} {last}" #f-string combinds the first and last name, to make a fullname
+
+#M = f"Hello,+{fullname.upper()}. How are you doing" #.upper, puts each and every leter into upper case (DAMIR DIEDUKHOV)
+#M1 = f"Hello,{fullname.lower()}. How are you doing" #.lower, puts each and every leter into lower case (damir diedukhov)
+#M2 = f"Hello,{fullname.title()}. How are you doing" #.titel, makes each and every first letter of a word upper case (Damir, Diedukhov)
+
+#print(M +"\n" + M1 + "\n" + M2) #"\n" makes a new clluom in the text 
+
+
+#print("'\t'python")  #In Python, \t represents the tab sequence for a horizontal tab character. When used within a string, \t is interpreted as a tab character and produces horizontal spacing.
+
+#text = "  Hello, World!  "
+
+#stripped_text = text.rstrip() # strips on the right of the text
+#stripped_text = text.lstrip() # strips on the left of the text
+#stripped_text = text.strip() # strips on both sides of the text
+#print(stripped_text)
+
+
+#3 + 2  #adding
+#3 - 2 #subtracting 
+#3 * 2 #multiplying
+#3 / 2 #dividing
+#3 ** 2 #you're putting the first number to the power of the last number 
+
+
+#first  = "Tim"
+#last = "Peters"
+#fullname = f"{first} {last}"
+
+#message = f"The Zen of Python, by {fullname.title()} \nBeautiful is better than ugly."
+#print(message)
---
+++ Everything-else/Lesson4.py
@@ -0,0 +1,86 @@
+#names = ["john", "peter", "smith"]
+
+#for name in names:  #In this example, the for loop iterates over each item in the names list, and the variable name takes the value of each item in each iteration. The print(name) statement prints each name on a separate line. The in operator, when used with a for loop, checks if there are more items in the sequence to iterate over. It helps in controlling the flow of the loop and ensures that all items in the sequence are processed.
+#    print(name)
+
+#names = ["john", "peter", "smith"]
+
+#for name in names:
+    #print(f"{name.title()} come over here!!!")
+#print(f"Can you buy me that bike for my birthday {name.title()}")
+
+#for value in range(0,11): #In Python, the range() function is used to generate a sequence of numbers. It returns an object of the range type that represents a sequence of numbers.
+#    print(value)
+
+#numbers = list(range(1,11)) #In Python, the list() function is used to create a new list object. It takes an iterable as an argument and returns a list containing the elements of the iterable.
+#print(numbers)
+
+#even_numbers = list(range(2, 11, 2)) #the first 2 adds up with another 2 till it comes to 11.
+#print(even_numbers)
+
+#squares = []
+#for value in range(1, 11):
+    #square = value ** 2
+    #squares.append(square) #each new value of square is appended to the list
+
+#print(squares)
+
+#square = [value**2 for value in range(1, 11)]
+#print(square) # same thing as above but in shorter terms
+
+#digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+#print(min(digits)) # the min function prints the smallest number in the list from them all.
+
+#digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+#print(max(digits)) # the max function prints the largest number in the list.
+
+#players = ['hilo', 'jupiter', 'hank', 'cricket', 'eli']
+#print(players[1:]) # the 1: prints everything after the first list name and not the first list name.
+
+#players = ['hilo', 'jupiter', 'hank', 'cricket', 'eli']
+#print(players[-3:])
+
+#players = ['hilo', 'jupiter', 'hank', 'cricket', 'eli']
+
+#print("Here are the first players on my team")
+#for player in players[3:]:
+    #print(player.title())
+
+
+#my_foods = ['pizza', 'burgers', 'salads', 'french fries']
+#friend_foods = my_foods[:-2]
+
+#print("\nMy favorite food are.")
+#print(my_foods)
+
+#print("\nMy friends favorite foods are.")
+#print(friend_foods)
+
+#my_foods = ['pizza', 'burgers', 'salads', 'french fries']
+#friend_foods = my_foods[:-2]
+
+#my_foods.append("tacos")
+#friend_foods.append("french fries")
+
+#print(my_foods)
+#print(friend_foods)
+
+#dimesion = (200, 50, 30)
+#print(dimesion[0])
+#print(dimesion[1])
+#print(dimesion[2])
+
+#dimesion[0] = 250
+
+#print(dimesion) # Doesn't work
+
+#dimensions = (200, 50)
+#print("\nDimesions")
+#for dimension in dimensions:
+    #print(dimension)
+
+#dimensions = (400, 30)
+#print("\nModified Dimesions")
+#for dimension in dimensions:
+    #print(dimension)
+
