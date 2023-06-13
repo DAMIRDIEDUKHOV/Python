ec2ef03bb5d9bd5079aebb49e06044a489805066
deddamir77 <deddamir77@gmail.com>
2023-06-13 09:54:25 -0500

    6/13

---
+++ Everything-else/Lesson6.py
@@ -0,0 +1,60 @@
+#allien_0 = {'color': 'green', 'points': 5, 'name': 'fkaghkadlrio'} # A dictionary in Python is an unordered collection of key-value pairs, where each key is unique within the dictionary. The keys are used to index and retrieve the corresponding values. Dictionaries are defined using curly braces {} and colons : to separate keys and values.
+
+#print(allien_0['color'])
+#print(allien_0['points'])
+#print(allien_0['name'])
+
+#allien_0 = {'color': 'green', 'points': 5}
+
+#extra_points = allien_0['points']
+#print(f"You've just arened {extra_points} points")
+
+#allien_0 = {'color': 'green', 'points': 5,}
+#print(allien_0)
+
+#allien_0['x_position'] = 25
+#allien_0['y_position'] = 0
+
+#print(allien_0)
+
+#allien = {}
+
+#allien['color'] = 'green'
+#allien['points'] = '5'
+
+#print(allien)
+
+
+#allien = {'color': 'green'}
+#print(f'The alliens color is {allien["color"]}.') 
+
+#allien['color'] = 'yellow'
+#print(f'Now the alliens color is {allien["color"]}!!!')
+
+#allien = {'x_position': 5, 'y_position': 0, 'speed': 'slow'}
+#print(f"The alliens x_position is at {allien['x_position']}")
+
+#if allien['speed'] == 'slow':
+    #speed_increment = 1
+#elif allien['speed'] == 'medium':
+    #speed_increment = 2
+#else:
+    #speed_increment = 3
+
+#allien['x_position'] = allien['x_position'] + speed_increment
+
+#print(f"The alliens position is now {allien['x_position']}") 
+
+#allien = {'color': 'green', 'points': 5}
+#print(allien)
+
+#del allien['color']
+#print(allien)
+
+favorite_video_games = {
+    'damir': 'rocketlegue'
+    'yehor': 'it_takes_two'
+    'eleonora': 'call_of_duty'
+    'daddy': 'tanks_of_trouble'
+    'mommy': 'nothing'
+    }
