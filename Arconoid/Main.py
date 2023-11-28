2fa85016b080c1458f2aab4764d121c0f0ac348d
DAMIRDIEDUKHOV <deddamir77@gmail.com>
2023-11-27 19:44:44 -0600

    11/27

--- Arconoid/Main.py
+++ Arconoid/Main.py
@@ -1,4 +1,7 @@
 import pygame
+import time
+import sys
+
 pygame.init()
 
 WIN_WIDTH = 800
@@ -10,14 +13,179 @@ window = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 pygame.display.set_caption('Arconoid')
 clock = pygame.time.Clock()
 
-game = True
+bg = pygame.image.load("Purple_backround.jpg")
+bg = pygame.transform.scale(bg, (WIN_WIDTH, WIN_HEIGHT))
 
+bg_win = pygame.image.load("you_win.jpg")
+bg_win = pygame.transform.scale(bg_win, (WIN_WIDTH, WIN_HEIGHT))
+
+bg_lose = pygame.image.load("you_lose.jpg")
+bg_lose = pygame.transform.scale(bg_lose, (WIN_WIDTH, WIN_HEIGHT))
+
+class GameSprites():
+    def __init__(self, x, y, width, height, file_name):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.image = pygame.image.load(file_name)
+        self.image = pygame.transform.scale(self.image, (width, height))
+        self.creation_time = time.time()  # Store the creation time of the health bar
+
+    def show_sprite(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+    def show(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+WIN_WIDTH = 800
+WIN_HEIGHT = 600
+FPS = 40
+BLACK = (0, 0, 0)
+
+window = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+pygame.display.set_caption('Arconoid')
+clock = pygame.time.Clock()
+
+font = pygame.font.Font(None, 36)
+
+bg = pygame.image.load("Purple_backround.jpg")
+bg = pygame.transform.scale(bg, (WIN_WIDTH, WIN_HEIGHT))
+
+bg_win = pygame.image.load("you_win.jpg")
+bg_win = pygame.transform.scale(bg_win, (WIN_WIDTH, WIN_HEIGHT))
+
+bg_lose = pygame.image.load("you_lose.jpg")
+bg_lose = pygame.transform.scale(bg_lose, (WIN_WIDTH, WIN_HEIGHT))
+
+class GameSprites():
+    def __init__(self, x, y, width, height, file_name):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.image = pygame.image.load(file_name)
+        self.image = pygame.transform.scale(self.image, (width, height))
+        self.creation_time = time.time()  # Store the creation time of the health bar
+
+    def show_sprite(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+    def show(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+platform = GameSprites(350, 500, 125, 100, "platform 2.png")
+ball = GameSprites(300, 300, 50, 50, "Ball.png")
+big_health_bar = GameSprites(180, 20, 175, 30, "full_health.png")
+x = 0
+enemies = []
+health_bars = []
+
+for i in range(9):
+    monster = GameSprites(x, 50, 75, 75, "monster.png")
+    enemies.append(monster)
+    x += 90
+
+x = 37
+for i in range(8):
+    monster = GameSprites(x, 140, 75, 75, "monster.png")
+    enemies.append(monster)
+    x += 90
+
+x = 75
+for i in range(7):
+    monster = GameSprites(x, 225, 75, 75, "monster.png")
+    enemies.append(monster)
+    x += 90
+x = 0
+for i in range(9):
+    health_bar = GameSprites(x, 130, 70, 10, "full_health.png")
+    health_bars.append(health_bar)
+    x += 90
+
+x = 37
+for i in range(8):
+    health_bar = GameSprites(x, 220, 70, 10, "full_health.png")
+    health_bars.append(health_bar)
+    x += 90
+
+x = 75
+for i in range(7):
+    health_bar = GameSprites(x, 310, 70, 10, "full_health.png")
+    health_bars.append(health_bar)
+    x += 90
+
+move_left = False
+move_right = False
+speed_x = 3
+speed_y = 3
+
+game = True
+level = 1
 while game:
+    current_time = time.time()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                move_left = True
+            if event.key == pygame.K_RIGHT:
+                move_right = True
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                move_left = False
+            if event.key == pygame.K_RIGHT:
+                move_right = False
 
-    window.fill(BLACK)
+    if level == 1:
+        window.blit(bg, (0, 0))
+        platform.show_sprite()
+        ball.show_sprite()
+        big_health_bar.show_sprite()
+
+        
+        for health_bar in health_bars.copy():
+            health_bar.show()
+
+
+            if current_time - health_bar.creation_time >= 2:
+                health_bars.remove(health_bar)
+
+        text = font.render('All Enemy Health:', True, (225, 225, 225))     
+        text_rect = text.get_rect(center=(70, 30))
+
+        window.blit(text, text_rect)
+
+        for enemy in enemies:
+            enemy.show_sprite()
+            if ball.rect.colliderect(enemy.rect):
+                enemies.remove(enemy)
+                speed_y = -speed_y
+
+        if move_left == True and platform.rect.left > 0:
+            platform.rect.x -= 10
+
+        if move_right == True and platform.rect.right < WIN_WIDTH:
+            platform.rect.x += 10
+
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if platform.rect.colliderect(ball.rect):
+            speed_y = -speed_y
+        if ball.rect.right >= WIN_WIDTH:
+            speed_x = -speed_x
+        if ball.rect.top <= 0:
+            speed_y = -speed_y
+        if ball.rect.left <= 0:
+            speed_x = -speed_x
+
+        if ball.rect.top > WIN_HEIGHT:
+            level = 3
+
+        if len(enemies) == 0:
+            level = 2
+
+    elif level == 2:
+        window.blit(bg_win, (0, 0))
+    elif level == 3:
+        window.blit(bg_lose, (0, 0))
 
     clock.tick(FPS)
     pygame.display.update()
--- Everything-else/RocketLegueSideswipe.py
+++ Everything-else/RocketLegueSideswipe.py
@@ -15,23 +15,20 @@ FPS = 60
 player_width = 50
 player_height = 25
 player_color = (255, 0, 0)
-player_x = WINDOW_WIDTH // 1.15 - player_width // 1.15
+player_x = WINDOW_WIDTH - 100  # Move the car further to the right
 player_y = WINDOW_HEIGHT - player_height
 player_speed_x = 5
 
-ball_radius = 30  # Adjust the size of the ball
+ball_radius = 30
 ball_color = (255, 255, 255)
 ball_x = WINDOW_WIDTH // 2
 ball_y = WINDOW_HEIGHT // 2
 ball_velocity_x = 0
 ball_velocity_y = 0
-gravity = 0.5  # Adjust the gravity value for desired effect
-bounce_factor = 0.8  # Adjust the bounce factor for desired effect
+gravity = 0.5
+bounce_factor = 0.8
 
-boundary_width = 30  # Width of the boundaries
-boundary_radius = 50  # Radius of the curved corners
-boundary_color = (0, 255, 0)  # Color of the boundaries
-boundary_thickness = 3  # Thickness of the boundary lines
+boundary_color = (0, 0, 255)  # Change boundary color
 
 while True:
     for event in pygame.event.get():
@@ -46,40 +43,34 @@ while True:
         player_x += player_speed_x
 
     player_rect = pygame.Rect(player_x, player_y, player_width, player_height)
-    ball_rect = pygame.Rect(ball_x - ball_radius, ball_y - ball_radius, ball_radius * 2, ball_radius * 2)  # Create a rectangle for the ball
 
-    ball_velocity_y += gravity  # Apply gravity to the ball's vertical velocity
-    ball_x += ball_velocity_x  # Update the ball's horizontal position based on velocity
-    ball_y += ball_velocity_y  # Update the ball's vertical position based on velocity
+    ball_velocity_y += gravity
+    ball_x += ball_velocity_x
+    ball_y += ball_velocity_y
 
-    if ball_y + ball_radius >= WINDOW_HEIGHT - boundary_width:  # Check collision with the bottom boundary
-        ball_y = WINDOW_HEIGHT - boundary_width - ball_radius  # Set the ball's position just above the bottom boundary
-        ball_velocity_y = -ball_velocity_y * bounce_factor  # Reverse the vertical velocity and apply bounce factor
+    if ball_y + ball_radius >= WINDOW_HEIGHT:
+        ball_y = WINDOW_HEIGHT - ball_radius
+        ball_velocity_y = -ball_velocity_y * bounce_factor
 
-    if ball_x - ball_radius <= boundary_width or ball_x + ball_radius >= WINDOW_WIDTH - boundary_width:
-        ball_velocity_x = -ball_velocity_x * bounce_factor  # Reverse the horizontal velocity and apply bounce factor
+    if ball_x - ball_radius <= 0 or ball_x + ball_radius >= WINDOW_WIDTH:
+        ball_velocity_x = -ball_velocity_x * bounce_factor
 
-    if ball_y - ball_radius <= WINDOW_HEIGHT - boundary_width:  # Check if the ball is above the boundary intercept line
-        if player_x - player_width // 2 < ball_x < player_x + player_width // 2:
-            ball_y = WINDOW_HEIGHT - boundary_width - ball_radius - player_height  # Set the ball's position just above the boundary intercept line
-            ball_velocity_y = -ball_velocity_y * bounce_factor  # Reverse the vertical velocity and apply bounce factor
+    if ball_y - ball_radius <= WINDOW_HEIGHT - player_height:
+        if player_rect.colliderect(ball_x - ball_radius, ball_y - ball_radius, ball_radius * 2, ball_radius * 2):
+            collision_direction = (ball_x - player_x - player_width / 2) / (player_width / 2)
+            ball_velocity_x = collision_direction * 5
+            ball_velocity_y = -ball_velocity_y
 
     window.fill((0, 0, 0))
 
-    # Draw the boundaries
-    pygame.draw.rect(window, boundary_color, (0, 0, WINDOW_WIDTH, boundary_width))  # Top boundary
-    pygame.draw.rect(window, boundary_color, (0, 0, boundary_width, WINDOW_HEIGHT - boundary_width))  # Left boundary
-    pygame.draw.rect(window, boundary_color, (0, WINDOW_HEIGHT - boundary_width, WINDOW_WIDTH, boundary_width))  # Bottom boundary
-    pygame.draw.rect(window, boundary_color, (WINDOW_WIDTH - boundary_width, 0, boundary_width, WINDOW_HEIGHT - boundary_width))  # Right boundary
-
-    # Draw the curved corners
-    pygame.draw.circle(window, boundary_color, (boundary_width, boundary_width), boundary_radius, boundary_thickness)  # Top left corner
-    pygame.draw.circle(window, boundary_color, (WINDOW_WIDTH - boundary_width, boundary_width), boundary_radius, boundary_thickness)  # Top right corner
-    pygame.draw.circle(window, boundary_color, (boundary_width, WINDOW_HEIGHT - boundary_width), boundary_radius, boundary_thickness)  # Bottom left corner
-    pygame.draw.circle(window, boundary_color, (WINDOW_WIDTH - boundary_width, WINDOW_HEIGHT - boundary_width), boundary_radius, boundary_thickness)  # Bottom right corner
+    # Draw the side boundaries with a different color
+    pygame.draw.rect(window, boundary_color, (0, 0, WINDOW_WIDTH, 30))  # Top boundary
+    pygame.draw.rect(window, boundary_color, (0, 0, 30, WINDOW_HEIGHT))  # Left boundary
+    pygame.draw.rect(window, boundary_color, (0, WINDOW_HEIGHT - 30, WINDOW_WIDTH, 30))  # Bottom boundary
+    pygame.draw.rect(window, boundary_color, (WINDOW_WIDTH - 30, 0, 30, WINDOW_HEIGHT))  # Right boundary
 
     pygame.draw.rect(window, player_color, player_rect)
-    pygame.draw.circle(window, ball_color, (ball_x, int(ball_y)), ball_radius)  # Draw the ball with integer position
+    pygame.draw.circle(window, ball_color, (ball_x, int(ball_y)), ball_radius)
 
     pygame.display.update()
     clock.tick(FPS)
